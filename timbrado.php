675698d23fcf578e8da59e0e23fe228355cf5090
Daniel Murillo <daniel.murillo@timbox.com.mx>
2017-11-08 14:46:19 -0600

    ADD metodo para generar sello y agregarlo al XML, y archivos necesarios para la generacion del mismo

--- timbrado.php
+++ timbrado.php
@@ -1,12 +1,55 @@
-/usr/bin/php7.0 -q
 <?php
+// Este metodo se utiliza para actualizar la fecha del XML, por lo que se requiere que
+// actualice el sello del mismo
+function actualizarSello($archivoXml){
+    //Leer XML
+    $xmlDoc = new DOMDocument();
+    $xmlDoc->load($archivoXml);
+    
+    //Cambiar Fecha a actual y guardar en archivo
+    date_default_timezone_set('America/Mexico_City');
+    $date = date('Y-m-d_H:i:s');
+    $date = str_replace("_", "T", $date);
+    $xmlDoc->firstChild->setAttribute('Fecha', $date);
+    $xmlString = $xmlDoc->saveXML();
+    file_put_contents($archivoXml, $xmlString);
+
+    //Crear cadena original
+    $xslt = new DOMDocument();
+    $xslt->load('cadenaoriginal_3_3.xslt');
+    $xml = new DOMDocument;
+    $xml->load($archivoXml);
+
+    $proc = new XSLTProcessor;
+    @$proc->importStyleSheet($xslt); // attach the xsl rules
+    $cadena = $proc->transformToXML($xml);
+    file_put_contents('cadena_original.txt', $cadena);
+    
+    //Firmar cadena y obtener el digest
+    $key = file_get_contents('CSD01_AAA010101AAA.key.pem');
+    openssl_sign($cadena, $digest, $key, OPENSSL_ALGO_SHA256);
+    file_put_contents('digest.txt', $digest);
+    
+    //Generar Sello
+    $command = "openssl enc -in digest.txt -out sello.txt -base64 -A -K CSD01_AAA010101AAA.key.pem";
+    $res = shell_exec($command);
+
+    //Actualizar el sello del XML
+    $sello = file_get_contents('sello.txt');
+    $xmlDoc->firstChild->setAttribute('Sello', $sello);
+    $xmlString = $xmlDoc->saveXML();
+    file_put_contents($archivoXml, $xmlString);    
+}
+
 //parametros para conexion al Webservice (URL de Pruebas)
 $wsdl_url = "https://staging.ws.timbox.com.mx/timbrado_cfdi33/wsdl";
 $wsdl_usuario = "AAA010101000";
 $wsdl_contrasena = "h6584D56fVdBbSmmnB";
 $ruta_xml = "archivoXml.xml";
 
-#convertir la cadena del xml en base64
+
+actualizarSello($ruta_xml);
+// convertir la cadena del xml en base64
 $documento_xml = file_get_contents($ruta_xml);
 $xml_base64 = base64_encode($documento_xml);
 
